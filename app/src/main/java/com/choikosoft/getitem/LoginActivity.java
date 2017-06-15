cad6461d7cd5e152879fa462524619cdd9879d0b
wanteder <khk5212@gmail.com>
2017-06-15 15:15:57 +0900

    특템 프로젝트 생성

---
+++ app/src/main/java/com/choikosoft/getitem/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.choikosoft.getitem;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by KHK on 2017-06-15.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
